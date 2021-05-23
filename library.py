2ff0a55fb02ee7043fd8d11bfe657e880fb10b15
enczilada88 <83005780+enczilada88@users.noreply.github.com>
2021-05-23 22:50:00 +0200

    Update library.py

--- library.py
+++ library.py
@@ -3,7 +3,7 @@ from datetime import date
 
 
 class Movie:
-    def __init__(self, title: str, year: int, genre: int) -> str:
+    def __init__(self, title, year, genre):
         self.title = title
         self.year = year
         self.genre = genre
@@ -11,16 +11,16 @@ class Movie:
         # Variables
         self.views = 0
 
-    def __str__(self)-> str:
+    def __str__(self):
         return f"{self.title} ({self.year})"
 
-    def play(self)-> str:
+    def play(self):
         self.views += 1
         return print(self)
 
 
 class Series(Movie):
-    def __init__(self, season: int, episode: int, *args, **kwargs):
+    def __init__(self, season, episode, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.season = season
         self.episode = episode
@@ -42,12 +42,12 @@ class LibraryAll:
         ]
 
     def get_movies(self):
-        self.movies_only = [item for item in self.library_database if (isinstance(item, Movie) and not isinstance(item, Series))]
-        return sorted(self.movies_only, key=lambda movie: movie.title)
+        movies_only = [item for item in self.library_database if (isinstance(item, Movie) and not isinstance(item, Series))]
+        return sorted(movies_only, key=lambda movie: movie.title)
 
     def get_series(self):
-        self.series_only = [item for item in self.library_database if isinstance(item, Series)]
-        return sorted(self.series_only, key=lambda series: series.title)
+        series_only = [item for item in self.library_database if isinstance(item, Series)]
+        return sorted(series_only, key=lambda series: series.title)
 
     def search(self, keyword):
         keyword=input("What movie are you searching for in Library?-provide title & check the status:")
@@ -58,19 +58,20 @@ class LibraryAll:
 
     def generate_views(self):
         self.library_database[random.randint(0, len(self.library_database)-1)].views += random.randint(100, 1000)
-        return True
+        return
 
     def top_titles(self, content_type=None, top_counter=3):
-        if content_type == None:
+        top_titles=[]
+        if content_type is None:
             return sorted(self.library_database, key=lambda movie: movie.views, reverse=True)[0:top_counter]
         elif content_type == "Movie":
-            self.movies_only = [item for item in self.library_database if (isinstance(item, Movie) and not isinstance(item, Series))]
-            return sorted(self.movies_only, key=lambda movie: movie.views, reverse=True)[0:top_counter]
+            movies_only = [item for item in self.library_database if (isinstance(item, Movie) and not isinstance(item, Series))]
+            return sorted(movies_only, key=lambda movie: movie.views, reverse=True)[0:top_counter]
         elif content_type == "Series":
-            self.series_only = [item for item in self.library_database if isinstance(item, Series)]
-            return sorted(self.series_only, key=lambda movie: movie.views, reverse=True)[0:top_counter]
+            series_only = [item for item in self.library_database if isinstance(item, Series)]
+            return sorted(series_only, key=lambda movie: movie.views, reverse=True)[0:top_counter]
         else:
-            return 0
+            return top_titles
 
 
 if __name__ == "__main__":
@@ -107,5 +108,10 @@ if __name__ == "__main__":
 
     print(f"The most popular series of {date.today()}:\n")
     for item in libAll.top_titles(content_type="Series", top_counter=3):
+        print(f"{item.title}: {item.views}")
+    print('-' * 100)
+# check of contant_type != series/!=movies
+    print(f"The most popular series of {date.today()}:\n")
+    for item in libAll.top_titles(content_type="check", top_counter=3):
         print(f"{item.title}: {item.views}")
     print('-' * 100)
